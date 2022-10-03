f9d9258ce6dcdcba403a2be43a478eead77a45dc
Kamil <kkrinatovich@gmail.com>
2022-10-03 13:32:06 +0300

    completed project

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -17,6 +17,17 @@ class CharList extends Component {
         charEnded: false,
         windowScrollListener: true
     }
+
+    itemRefs = [];
+
+    setRef = (e) => {
+        this.itemRefs.push(e);
+    }
+
+    onFocus = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+    }
     
     marvelService = new MarvelService();
 
@@ -78,7 +89,7 @@ class CharList extends Component {
     }
 
     renderItems(arr) {
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
@@ -88,7 +99,18 @@ class CharList extends Component {
                 <li 
                     className="char__item"
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    ref={this.setRef}
+                    tabIndex={0}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.onFocus(i);
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.code === 'Enter') {
+                            this.props.onCharSelected(item.id);
+                            this.onFocus(i);
+                        }
+                    }} >
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
